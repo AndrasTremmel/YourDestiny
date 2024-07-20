1297b703315a744edadf345ddd7f05824f9b6cc0
PeterOri <oripeter15@gmail.com>
2024-07-20 19:28:16 +0200

    RandomGenerator
    A class for getting a random Object from a list with different probabilities.

---
+++ core/src/com/teeles/tol/model/RandomElement.java
@@ -0,0 +1,11 @@
+package com.teeles.tol.model;
+
+public class RandomElement {
+    public Object element;
+    public double probability;
+
+    public RandomElement(Object element, double probability) {
+        this.element = element;
+        this.probability = probability;
+    }
+}
---
+++ core/src/com/teeles/tol/model/RandomGenerator.java
@@ -0,0 +1,28 @@
+package com.teeles.tol.model;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomGenerator {
+    double fullProb = 0;
+    private Random rand = new Random();
+    private List<RandomElement> results = new LinkedList<>();
+
+    public void AddResult(Object result, double prob) {
+        results.add(new RandomElement(result, prob));
+        fullProb += prob;
+    }
+
+    public Object GetElement() {
+        if (results.isEmpty()) { throw new RuntimeException("Cannot get random result from zero long result list."); }
+        double randomNumber = rand.nextDouble(fullProb);
+        double currSum = 0.0;
+        int i = 0;
+        do {
+            currSum = currSum + results.get(i).probability;
+            i++;
+        } while (currSum < randomNumber);
+        return results.get(i).element;
+    }
+}
